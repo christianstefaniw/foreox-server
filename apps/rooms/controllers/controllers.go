90c2e9511252b849adf6d44081ad2707a50e3f8f
BenReichwein <ben.basque@icloud.com>
2021-05-03 10:15:30 -0400

    commit

--- apps/rooms/controllers/controllers.go
+++ apps/rooms/controllers/controllers.go
@@ -44,7 +44,6 @@ func RoomInfo(c *gin.Context) {
 func AllUsersRooms(c *gin.Context) {
 	var allRooms []rooms.Room
 	user, _ := c.Get("user")
-
 	for _, roomId := range user.(*accounts.User).Rooms {
 		var rm rooms.Room
 		database.Database.FindOne(context.Background(), constants.ROOMS_COLL, bson.M{"_id": roomId}).Decode(&rm)
